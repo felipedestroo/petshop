534364936f3612933dedaf01e6db18bab65c75f9
felipedestroo <fdestrocontact@gmail.com>
2025-09-03 16:27:31 -0300

    fix: Correct duration property typos and enhance Services component layout with new icons and contact button

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import useEmblaCarousel from "embla-carousel-react";
-import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from "lucide-react";
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from "lucide-react";
 import { WhatsappLogoIcon } from "@phosphor-icons/react";
 
 const services = [
   {
       title: "Banho & Tosa",
       description: "Inclui banho com produtos específicos para o tipo de pelagem e pele do animal. Corte de unhas, limpeza das orelhas e tosa personalizada (higiênica ou estilizada).",
-      duartion: "1h",
+      duration: "1h",
       price: "R$50",
       icon: <Scissors />,
       linkText: "Olá, vi no site sobre Banho e tosa e gostaria de mais informações."
@@ -16,15 +16,15 @@ const services = [
   {
     title: "Consulta Veterinária",
     description: "Oferece atendimento clínico básico ou especializado para cuidar da saúde do animal. Inclui diagnóstico de doenças, aplicação de vacinas obrigatórias.",
-    duartion: "1h",
+    duration: "1h",
     price: "R$45",
     icon: <Syringe />,
     linkText: "Olá, vi no site sobre consulta veterinária e gostaria de mais informações."
   },
   {
     title: "Táxi Pet",
-    description: "Serviço de transporte para levar e buscar os pets no petshop, clínicas veterinárias ou outros locais. Ideal para tutores que não têm tempo ou transporte adequado para locomover os animais,",
-    duartion: "2h",
+    description: "Serviço de transporte para levar e buscar os pets no petshop, clínicas veterinárias ou outros locais. Ideal para tutores que não têm tempo ou transporte adequado para locomover os animais.",
+    duration: "2h",
     price: "R$80",
     icon: <CarTaxiFront />,
     linkText: "Olá, vi no site sobre táxi pet e gostaria de mais informações."
@@ -32,7 +32,7 @@ const services = [
   {
     title: "Hotel para pets",
     description: "Serviço de hospedagem para animais de estimação, ideal para quando os tutores precisam viajar ou se ausentar por alguns dias. Os pets ficam acomodados em espaços seguros e confortáveis.",
-    duartion: "A Combinar",
+    duration: "A Combinar",
     price: "A Combinar",
     icon: <Hotel />,
     linkText: "Olá, vi no site sobre hotel para pets e gostaria de mais informações."
@@ -48,7 +48,15 @@ export function Services() {
     breakpoints: {
       "(min-width: 768px)": { slidesToScroll: 3 }
     }
-  }); 
+  });
+  
+  function scrollPrev() {
+    emblaApi?.scrollPrev();
+  }
+
+  function scrollNext() {
+    emblaApi?.scrollNext();
+  }
   
   return(
     <section className="bg-white py-16">
@@ -65,7 +73,7 @@ export function Services() {
                 <div key={index} className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3">
                   <article className="bg-[#1E293B] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
                     <div className="flex-1 flex items-start justify-between">
-                      <div>
+                      <div className="flex gap-3">
                         <span className="text-3xl">{item.icon}</span>
                         <div>
                           <h3 className="font-bold text-xl my-1 select-none">{item.title}</h3>
@@ -75,11 +83,44 @@ export function Services() {
                         </div>
                       </div>
                     </div>
+
+                    <div className="border-t border-gray-700 pt-4 flex items-center justify-between">
+                      <div className="flex items-center gap-2 text-sm">
+                        <Clock className="w-4 h-4"/>
+                        <span>{item.duration}</span>
+                      </div>
+
+                      <a 
+                        href="#"
+                        className="flex items-center justify-center gap-2 px-4 py-1 rounded-md hover:bg-[#1f2e46] duration-300"
+                      >
+                        <WhatsappLogoIcon className="w-5 h-5"/>
+                        Entrar em contato
+                      </a>
+                    </div>
                   </article>
                 </div>
               ))}
             </div>
           </div>
+
+          <button 
+            className="cursor-pointer bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+            onClick={scrollPrev}
+          >
+            <ChevronLeft 
+              className="w-6 h-6 text-gray-600"
+            />
+          </button>
+
+          <button 
+            className="cursor-pointer bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+            onClick={scrollNext}
+          >
+            <ChevronRight
+              className="w-6 h-6 text-gray-600"
+            />
+          </button>
         </div>
       </div>
     </section>
